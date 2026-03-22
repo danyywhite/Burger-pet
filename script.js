c79e2776807f68be9efcb4f5a68b571cea8f8701
danyywhite <sultanbekovdanil@gmail.com>
2026-03-22 22:38:44 +0300

    +js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-a = "ПРИВЕТ ДРУГ!"
-alert(a)
+document.addEventListener("DOMContentLoaded", function() {
+    //  кнопки "Order now"
+    const orderButtons = document.querySelectorAll('.header .btn, .order .btn');
+
+    // секцию продукта
+    const productSection = document.getElementById('product');
+
+    orderButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.preventDefault(); // Отключаем стандартное поведение ссылки
+
+            // Прокручиваем к секции продукта
+            productSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
